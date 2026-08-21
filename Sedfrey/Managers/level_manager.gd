df117ecfa4d4520f6acd9f4ec382f7a871bb47fc
ztacio <zacharytacio@gmail.com>
2026-08-21 17:45:50 -0600

    menu goes to stage now

--- Sedfrey/Managers/level_manager.gd
+++ Sedfrey/Managers/level_manager.gd
@@ -1,9 +1,11 @@
 extends Node
 
 const SCENES = {
-	"melo": preload("res://Sedfrey/Boss/Melo/Map/melo_stage.tscn")
+	"melo": preload("res://scenes/stage.tscn")
 }
 
+#res://Sedfrey/Boss/Melo/Map/melo_stage.tscn
+#res://scenes/stage.tscn
 var curr_scene: Node2D
 
 func _ready() -> void:
